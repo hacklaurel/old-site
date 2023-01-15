966eb2583afca42cc5ec5122a195257268d7c151
te <thrizzle@skiff.com>
2023-01-14 22:39:45 -0500

    📈 add analytics

--- pages/_document.js
+++ pages/_document.js
@@ -13,6 +13,7 @@ export default class extends Document {
         <Head>
           <style>{`:root { supported-color-schemes: light dark; }`}</style>
           <meta name="format-detection" content="telephone=no" />
+          <script async src="https://hacklaurel-analytics.up.railway.app/tracker.js" data-ackee-server="https://hacklaurel-analytics.up.railway.app" data-ackee-domain-id="7caaf1ec-81b8-451c-af13-17f999f3a1f4"></script>
         </Head>
         <body>
           <InitializeColorMode />
